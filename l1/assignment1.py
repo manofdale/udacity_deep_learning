da25a8f587e958e947455a05968b98b42a719a47
A. G. Polat <agplt@hotmail.com>
2016-01-28 13:55:44 +0200

    imshow training and test set images and their corresponding labels

--- l1/assignment1.py
+++ l1/assignment1.py
@@ -67,3 +67,84 @@ def show_an_image(folders):
 
 show_an_image(train_folders)
 show_an_image(test_folders)
+
+"""" End of Problem 1 """""
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+
+def load(data_folders, min_num_images, max_num_images):
+    dataset = np.ndarray(
+            shape=(max_num_images, image_size, image_size), dtype=np.float32)
+    labels = np.ndarray(shape=(max_num_images), dtype=np.int32)
+    label_index = 0
+    image_index = 0
+    for folder in data_folders:
+        print folder
+        for image in os.listdir(folder):
+            if image_index >= max_num_images:
+                raise Exception('More images than expected: %d >= %d' % (
+                    image_index, max_num_images))
+            image_file = os.path.join(folder, image)
+            try:
+                image_data = (ndimage.imread(image_file).astype(float) -
+                              pixel_depth / 2) / pixel_depth
+                if image_data.shape != (image_size, image_size):
+                    raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+                dataset[image_index, :, :] = image_data
+                labels[image_index] = label_index
+                image_index += 1
+            except IOError as e:
+                print 'Could not read:', image_file, ':', e, '- it\'s ok, skipping.'
+        label_index += 1
+    num_images = image_index
+    dataset = dataset[0:num_images, :, :]
+    labels = labels[0:num_images]
+    if num_images < min_num_images:
+        raise Exception('Many fewer images than expected: %d < %d' % (
+            num_images, min_num_images))
+    print 'Full dataset tensor:', dataset.shape
+    print 'Mean:', np.mean(dataset)
+    print 'Standard deviation:', np.std(dataset)
+    print 'Labels:', labels.shape
+    return dataset, labels
+
+
+train_dataset, train_labels = load(train_folders, 450000, 550000)
+test_dataset, test_labels = load(test_folders, 18000, 20000)
+
+"""Problem 2
+
+Let's verify that the data still looks good. Displaying a sample of the labels and images from the ndarray.
+Hint: you can use matplotlib.pyplot."""
+
+
+def plot_an_image(dataset_img, label):
+    plt.imshow(dataset_img)
+    print(str(label))
+    plt.title(str(label))
+    display(plt.show())
+
+
+plot_an_image(test_dataset[19], test_labels[19])
+plot_an_image(test_dataset[15000], test_labels[15000])
+plot_an_image(test_dataset[8900], test_labels[8900])
+plot_an_image(train_dataset[19], train_labels[19])
+plot_an_image(train_dataset[135000], train_labels[135000])
+plot_an_image(train_dataset[28900], train_labels[28900])
+
+"""" End of Problem 2 """""
+
+np.random.seed(133)
+
+
+def randomize(dataset, labels):
+    permutation = np.random.permutation(labels.shape[0])
+    shuffled_dataset = dataset[permutation, :, :]
+    shuffled_labels = labels[permutation]
+    return shuffled_dataset, shuffled_labels
+
+
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
